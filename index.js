6c89bb2f499a7144360a6ca598f0ea8384602372
Edmier <kameron@edmier.com>
2021-12-03 10:17:22 -0500

    Better fill bars

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const skills = Array.from(document.getElementsByClassName('lang'));
 const year = new Date().getFullYear();
+let biggest = 0;
+
+skills.forEach(skill => {
+    biggest = Math.max(year - skill.dataset.startyear, biggest);
+});
+
+console.log(biggest);
+
 skills.forEach(skill => {
     const startyear = skill.dataset.startyear;
+
     const fill = document.createElement('div');
     fill.classList.add('fill');
-    fill.style.width = ((year - startyear) * 10) + '%';
+    fill.style.width = ((year - startyear) / biggest * 100) + '%';
 
     const pre = document.createElement('pre');
     pre.innerText = (year - startyear) + " years";
-    pre.style.left = ((year - startyear) * 10) + '%';
+    if (+fill.style.width.replace('%', '') > 80) {
+        pre.style.textAlign = 'left';
+        pre.style.left = ((year - startyear) / biggest * 85) + '%';
+        pre.style.color = 'black';
+    } else {
+        pre.style.left = ((year - startyear) / biggest * 100) + '%';
+    }
+    
 
     skill.appendChild(fill);
     skill.appendChild(pre);
